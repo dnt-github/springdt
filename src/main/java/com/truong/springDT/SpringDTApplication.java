964dc7c5e117525930a6a4d54f6ac2cb06c78721
dtruong <dtruong3@yahoo.com>
2019-12-28 01:13:23 -0500

    Initial Commit for my Spring boot Reddit Clone.

---
+++ src/main/java/com/truong/springDT/SpringDTApplication.java
@@ -0,0 +1,13 @@
+package com.truong.springDT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDTApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/truong/springDT/SpringdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truong.springDT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
